3eb32b19769ff1b2c7034f1c2e8188de0a420188
Alexamith <samirportillo231099@gmail.com>
2021-01-29 12:21:53 -0600

    Configuracion de las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Router, RouterModule } from '@angular/router';
+import { Route } from './app.routes';
+
+
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistaComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Route, { useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
